65860e2dc19aaa1dc0b54fc9d31c17be2921f0a4
Marco N Antonio <marco.antonio@outlook.com>
2022-04-30 13:09:02 -0700

    Initial Commit

---
+++ SourceControl1/SourceControl1App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControl1App.swift
+//  SourceControl1
+//
+//  Created by Marco Antonio on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControl1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
