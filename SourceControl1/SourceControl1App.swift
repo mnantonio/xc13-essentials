7b7d7ef4fa99858d3236942959ee4cd3d1e7948e
Marco N Antonio <marco.antonio@outlook.com>
2022-04-30 13:43:24 -0700

    Adding missed code fix

--- SourceControl1/SourceControl1App.swift
+++ SourceControl1/SourceControl1App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SourceControl1App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()  //fixed bug b
+            ContentView()  // fixed bug b
         }
     }
 }
